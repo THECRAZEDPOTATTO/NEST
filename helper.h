af0a9c4492d0f73009f5a91ed10886a3632b0259
Crazedpotato <92868941+THECRAZEDPOTATTO@users.noreply.github.com>
2022-09-15 19:58:08 -0400

    Update helper.h

--- helper.h
+++ helper.h
@@ -1,9 +1,27 @@
-#include "iostream"
+#pragma once
+#include <Windows.h>
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
-void procceschecker()
-{
-    
+#include <string>
+#include <VersionHelpers.h>
+void runchcker() {
+printf("checking for EXE\n");
+	
 }
+void oschecker() {
+	printf("checking for OS\n");
+	if (!IsWindows10OrGreater())
+	{
+		printf("Meets Requirements\n");
+		IsWindowsXPSP1OrGreater();
+
+	}else{
+		printf("Error");
+		exit(1);
+	}
+
+}
+void luachecker() {
+printf("checking for Lua\n");
+
+}
+
