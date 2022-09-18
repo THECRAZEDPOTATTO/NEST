6d9a10d2b5dfd691c98a03178e7d355fcc7c73b8
Crazedpotato <92868941+THECRAZEDPOTATTO@users.noreply.github.com>
2022-09-17 22:47:45 -0400

    Update helper.h

--- helper.h
+++ helper.h
@@ -17,7 +17,7 @@ LPCSTR app_name = "Roblox";
 if (FindWindowA(0, app_name)) {
 	printf("Roblox Found\n");
 }else {
-	std::this_thread::sleep_for(std::chrono::milliseconds(3000));    // sleep for 1 second
+	std::this_thread::sleep_for(std::chrono::milliseconds(3000));    // sleep for 3 second
 	system("taskkill /F /T /IM Nest.exe");
 	MessageBox(NULL, L"Not Found", L"Roblox Not Found", MB_OKCANCEL);
 	printf("Roblox Not Found\n");
